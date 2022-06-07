dc94256e2ad8d23434a9828f7f0c549230870c84
CJL Fortunato <clemence.fortunato@gmail.com>
2022-06-07 15:38:41 +0200

    Replace list in NbLaunches by a chart

--- src/App/Components/Other_components/NbLaunches.js
+++ src/App/Components/Other_components/NbLaunches.js
@@ -1,22 +1,41 @@
 import React from "react";
+import { Chart, registerables } from "chart.js";
 
 import { getLaunchesByYear } from "../../Utils/helperFunctions";
+import { Bar } from "react-chartjs-2";
+
+Chart.register(...registerables);
 
 export function NbLaunches(props) {
 
 
     const totalLaunches = props ? props.data.totalDocs : "?";
 
-    const data = props.data.docs? props.data.docs : props.data;
+    const data = props.data.docs ? props.data.docs : props.data;
 
     const dataset = getLaunchesByYear(data);
 
+    const chartData = {
+        labels: Object.keys(dataset),
+        datasets: [
+            {
+                label: "Launches per year",
+                backgroundColor: 'dodgerblue',
+                borderColor: 'dodgerblue',
+                data: Object.values(dataset),
+            }
+        ]
+    }
 
 
-    return ( 
+
+    return (
         <div className="nb-launches">
             <h2>Number of launches: <span>{totalLaunches}</span></h2>
-            <ul>{Object.keys(dataset).map(year => <li key={year}><strong>{year}: </strong>{dataset[year]}</li>)}</ul>
-        </div> 
+            <div className="chart">
+                <Bar data={chartData} />
+            </div>
+
+        </div>
     );
 }
